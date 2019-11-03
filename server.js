387b9d81b60b2d6c09cfc05bf4a6c9404b4d2a4a
bdange <benjamin.dange@gmail.com>
2019-11-03 21:31:40 +0100

    deployment

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ app.use("/api/items", items);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server started on ${port}`));
+app.listen(port, () => console.log(`Server started on ${port}`));
